23cabd8e0a5650ab972dbc890245aec532deaae3
owobo <owobo@DESKTOP-1PHN480.hitronhub.home>
2021-05-25 01:07:09 -0600

    First Commit lAB 2

--- src/helloworldlab2/HelloWorldLab2.java
+++ src/helloworldlab2/HelloWorldLab2.java
@@ -16,6 +16,7 @@ public class HelloWorldLab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Practice for Lab two");
     }
     
 }
